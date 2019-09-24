422e2d6052b0066fb65013378ca2b55df083e8b3
ygm <756949055@qq.com>
2019-09-24 11:23:45 +0800

    add edit css

--- client/common/components/form-table/EditBlock.tsx
+++ client/common/components/form-table/EditBlock.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import CoreComponent from 'art-lib-react/src/core/CoreComponent';
 import SearchInput from '../search-input';
+import './style/edit.block.less';
 
 export default class EditBlock extends CoreComponent<any, any> {
   public render() {
@@ -9,8 +10,10 @@ export default class EditBlock extends CoreComponent<any, any> {
       <div className="title">edit node</div>
       <div className="input-block name-input">name: <SearchInput /> </div>
       <div className="input-block type-input">type: <SearchInput /> </div>
-      <div className="input-block explain-input">explain: <SearchInput /> </div>
-      <div className="input-block example-input">example: <SearchInput /> </div>
+      <div>
+        <div className="input-block explain-input">explain: <SearchInput /> </div>
+        <div className="input-block example-input">example: <SearchInput /> </div>
+      </div>
       <div className="input-block enum-input">enum: <SearchInput /> </div>
       <div className="input-block parents-input">parents: <SearchInput /> </div>
     </div>);
