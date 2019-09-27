9911f5cafe0709f3fe53111211032fcab2715d66
ygm <756949055@qq.com>
2019-09-27 14:47:52 +0800

    add edit reg

--- client/common/components/form-table/EditBlock.tsx
+++ client/common/components/form-table/EditBlock.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import CoreComponent from 'art-lib-react/src/core/CoreComponent';
 import SearchInput from '../search-input';
 import './style/edit.block.less';
+import { IChangeValueInfo } from '../search-input/propsType';
 
 const TYPE_LIST = ['string', 'number', 'int', 'float', 'boolean', 'array', 'array(string)', 'object',
   'array(object)'];
@@ -14,18 +15,40 @@ enum ValueType {
   enum = 'enum',
   parent = 'parent'
 }
+
+const NAME_DEFAULT_STATUS = false;
+const TYPE_DEFAULT_STATUS = false;
+const ENUM_DEFAULT_STATUS = true;
+const PARENT_DEFAULT_STATUS = false;
 export default class EditBlock extends CoreComponent<any, any> {
   public state = {
     nameValue: '',
+    nameValueStatus: NAME_DEFAULT_STATUS,
     typeValue: '',
+    typeValueStatus: TYPE_DEFAULT_STATUS,
     explainValue: '',
     exampleValue: '',
     enumValue: '',
-    parentValue: ''
+    enumValueStatus: ENUM_DEFAULT_STATUS,
+    parentValue: '',
+    parentValueStatus: PARENT_DEFAULT_STATUS
   };
 
-  public verifyName = (nameValue) => {
+  public verifyName = (nameValue: string) => {
     return this.isLegalVariableName(nameValue);
+  }
+
+  public verifyEnum = (enumValue: string) => {
+    if (enumValue === '') { return true; }
+    return this.isEnumStructure(enumValue);
+  }
+
+  public verifyParent = (parentValue: string) => {
+    return this.isParentStructure(parentValue);
+  }
+
+  public verifyDefault = () => {
+    return true;
   }
 
   public formatName = (nameValue) => {
@@ -39,20 +62,23 @@ export default class EditBlock extends CoreComponent<any, any> {
 
   public isCorrect = () => {
     const state = this.state;
-    if (this.verifyName(state.nameValue)) {
-      return true;
-    }
-    return false;
+    return state.nameValueStatus && state.typeValueStatus && state.enumValueStatus && state.parentValueStatus;
   }
 
-  public onChangeValue = (type: ValueType, value: string) => {
-    console.log(type, value);
+  public onChangeValue = (type: ValueType, valueInfo: IChangeValueInfo) => {
+    const { value, isCorrect } = valueInfo;
     switch (type) {
       case ValueType.name:
-        this.setState({ nameValue: value });
+        this.setState({
+          nameValue: value,
+          nameValueStatus: isCorrect
+        });
         break;
       case ValueType.type:
-        this.setState({ typeValue: value });
+        this.setState({
+          typeValue: value,
+          typeValueStatus: isCorrect
+        });
         break;
       case ValueType.explain:
         this.setState({ explainValue: value });
@@ -61,10 +87,16 @@ export default class EditBlock extends CoreComponent<any, any> {
         this.setState({ exampleValue: value });
         break;
       case ValueType.enum:
-        this.setState({ enumValue: value });
+        this.setState({
+          enumValue: value,
+          enumValueStatus: isCorrect
+        });
         break;
       case ValueType.parent:
-        this.setState({ parentValue: value });
+        this.setState({
+          parentValue: value,
+          parentValueStatus: isCorrect
+        });
         break;
     }
   }
@@ -76,6 +108,16 @@ export default class EditBlock extends CoreComponent<any, any> {
   private isLegalVariableName = (str: string) => {
     const variableNameReg = /^[a-zA-Z\$_][a-zA-Z\d_]*$/;
     return variableNameReg.test(str);
+  }
+
+  private isEnumStructure = (str: string) => {
+    const enumValueReg = /^([a-zA-Z\$_][a-zA-Z\d_]*=['"]?[\d\w$]+['"]?,?)+$/;
+    return enumValueReg.test(str);
+  }
+
+  private isParentStructure = (str: string) => {
+    const parentValueReg = /^([a-zA-Z\$_][a-zA-Z\d_]*.?)*([a-zA-Z\$_][a-zA-Z\d_]*)$/;
+    return parentValueReg.test(str);
   }
 
   public render() {
@@ -99,22 +141,26 @@ export default class EditBlock extends CoreComponent<any, any> {
           <div className="input-block explain-input">
             explain:<SearchInput
               onChangeValue={this.onChangeValue.bind(this, ValueType.explain)}
+              verifyValue={this.verifyDefault}
             />
           </div>
           <div className="input-block example-input">
             example:<SearchInput
               onChangeValue={this.onChangeValue.bind(this, ValueType.example)}
+              verifyValue={this.verifyDefault}
             />
           </div>
         </div>
         <div className="input-block enum-input">
           enum:<SearchInput
             onChangeValue={this.onChangeValue.bind(this, ValueType.enum)}
+            verifyValue={this.verifyEnum}
           />
         </div>
         <div className="input-block parents-input">
           parents:<SearchInput
             onChangeValue={this.onChangeValue.bind(this, ValueType.parent)}
+            verifyValue={this.verifyParent}
           />
         </div>
         <div className="edit-btn-block">
